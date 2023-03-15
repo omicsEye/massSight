1ed32733cece40abe9b52e7ad8e2f7bf20b9e323
Chiraag <37530396+chi-raag@users.noreply.github.com>
2023-03-15 13:22:08 -0400

    updated consolidate function

--- R/consolidate.R
+++ R/consolidate.R
@@ -22,17 +22,18 @@ consolidate <-
            use_mz = T,
            rt_threshold = NULL,
            mz_threshold = NULL) {
+    stopifnot(!massSight_obj@consolidated, "Data is already consolidated")
     df <- massSight_obj@raw
     if (use_rt) {
       stopifnot(!is.null(rt_threshold),
                 "If `use_rt` is true, `rt_threshold` must be defined")
-      df <- df |> arrange(RT)
+      df <- df |> arrange(rt)
       i <- 1
       rt_adducts <- c()
       while (i <= nrow(df)) {
         j <- i + 1
-        while (df[j, "RT"] - df[i, "RT"] <= rt_threshold) {
-          rt_adducts <- rt_adducts |> c(j)
+        while (df[j, "rt"] - df[i, "rt"] <= rt_threshold) {
+          rt_adducts <- rt_adducts |> c(df[j, "id"])
           j <- j + 1
           if (j > nrow(df)) {
             break
@@ -44,13 +45,13 @@ consolidate <-
     if (use_mz) {
       stopifnot(!is.null(mz_threshold),
                 "If `use_mz` is true, `mz_threshold` must be defined")
-      df <- df |> arrange(MZ)
+      df <- df |> arrange(mz)
       i <- 1
       mz_adducts <- c()
       while (i < nrow(df)) {
         j <- i + 1
-        while (df[j, "MZ"] - df[i, "MZ"] <= mz_threshold) {
-          mz_adducts <- mz_adducts |> c(j)
+        while (df[j, "mz"] - df[i, "mz"] <= mz_threshold) {
+          mz_adducts <- mz_adducts |> c(df[j, "id"])
           j <- j + 1
           if (j > nrow(df)) {
             break
@@ -60,12 +61,24 @@ consolidate <-
       }
     }
     if (use_mz & use_rt) {
-      df <- df[-(intersect(rt_adducts, mz_adducts))]
+      df <- df |>
+        dplyr::filter(!(id %in% intersect(rt_adducts, mz_adducts)))
     } else if (use_rt) {
-      df <- df[-rt_adducts]
+      df <- df |>
+        dplyr::filter(!(id %in% rt_adducts))
     } else if (use_mz) {
-      df <- df[-mz_adducts]
+      df <- df |>
+        dplyr::filter(!(id %in% mz_adducts))
     }
-    massSight_obj@consolidated <- df
+    massSight_obj@raw <- df
+    massSight_obj@consolidated <- T
+
+    if (nrow(massSight_obj@metadata) != 0) {
+      metadata <- massSight_obj@metadata
+      metadata <- metadata |>
+        dplyr::filter(id %in% df$id)
+      massSight_obj@metadata <- metadata
+    }
+
     return(massSight_obj)
   }
