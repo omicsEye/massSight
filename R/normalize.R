d75e8a3f1fc716e39779fd64ae32dfcaaae78e77
Chiraag <37530396+chi-raag@users.noreply.github.com>
2023-09-21 16:55:31 -0400

    updated normalize

--- R/normalize.R
+++ R/normalize.R
@@ -41,20 +41,21 @@ smooth_normalize <- function(ndata,
   prefs_present <- !is.na(prefs_raw_data)
 
   prefs_information <- prefs_information |>
-    dplyr::arrange(Injection_order)
+    dplyr::arrange(.data$Injection_order)
   sample_injection_order <- sample_information$Injection_order
   ref_to_use <- sample_information$Ref_to_use
 
   normalized_data <- data.frame()
   nn_normalized <- c()
 
-  for (i in 1:nrow(ndata)) {
+  for (i in seq_len(nrow(ndata))) {
     key <- prefs_present[i, ]
     pool_names <- prefs_information$Collaborator_ID[key]
     pool_injections <- prefs_information$Injection_order[key]
     pool_values <- prefs_raw_data[i, key]
 
-    if ((1 - length(pool_names) / length(all_p_names)) * 100 >= pool_missing_p) {
+    if ((1 - length(pool_names) / length(prefs_information$Collaborator_ID)) *
+      100 >= pool_missing_p) {
       normalization_scalars <- ""
       skipped <- skipped + 1
     } else {
@@ -83,7 +84,8 @@ smooth_normalize <- function(ndata,
         next
       } else {
         normalization_scalars <-
-          pool_values[pool_to_use_ind] / median(pool_values[unique(pool_to_use_ind)])
+          pool_values[pools_to_use_ind] /
+            median(pool_values[unique(pools_to_use_ind)])
         normalized_data <- normalized_data |>
           rbind(ndata[i, ] / normalization_scalars)
         nn_normalized <- nn_normalized |>
@@ -91,6 +93,18 @@ smooth_normalize <- function(ndata,
         next
       }
     }
-    # if (smooth)
+    if (smooth_method == "line") {
+      smooth_x <- pool_injections[unique(pools_to_use_ind)]
+      smooth_y <- pool_values[unique(pools_to_use_ind)]
+    } else if (smooth_method == "lowess") {
+      lowess_fun <-
+        lowess(
+          pool_injections[unique(pools_to_use_ind)],
+          pool_values[unique(pools_to_use_ind)],
+          .2
+        )
+      smooth_x <- lowess_fun$x
+      smooth_y <- lowess_fun$y
+    }
   }
 }
