31af24f0fb6b34a32540f06759a1463b153dbfd4
Chiraag <37530396+chi-raag@users.noreply.github.com>
2024-03-12 12:30:33 -0400

    updated logging

--- R/logging.R
+++ R/logging.R
@@ -1,3 +1,23 @@
+check_jsonlite <- function() {
+  if (!requireNamespace("jsonlite", quietly = TRUE)) {
+    message("The 'jsonlite' package is required for logging.")
+    message("1: Install 'jsonlite")
+    message("2: Do not install and exit")
+    choice <- readline(prompt = "Enter your choice (1 or 2): ")
+
+    if (choice == "1") {
+      install.packages("jsonlite")
+      if (!requireNamespace("jsonlite", quietly = TRUE)) {
+        stop("Package 'jsonlite' failed to install. Please try installing manually.", call. = FALSE)
+      }
+    } else if (choice == "2") {
+      stop("Package 'jsonlite' is not installed.", call. = FALSE)
+    } else {
+      stop("Invalid choice. Please restart the function and enter 1 or 2.", call. = FALSE)
+    }
+  }
+}
+
 initialize_log <- function(call) {
   logr::log_open(
     paste0(
@@ -16,4 +36,12 @@ initialize_log <- function(call) {
     hide_notes = T
   )
   logr::log_print(call, console = F, hide_notes = T)
+}
+
+
+modify_call <- function(call) {
+  defaults <- as.list(formals(auto_combine))
+  combined <- modifyList(defaults, as.list(call)[-1])
+  call <- as.call(c(as.symbol(deparse(call[[1]])), combined))
+  return(call)
 }
