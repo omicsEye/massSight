901311af677e890d334a44a55a37c7d8d3ac1b03
Chiraag <37530396+chi-raag@users.noreply.github.com>
2024-07-29 14:53:03 -0400

    updated test, helper refactoring

--- R/auto_combine.R
+++ R/auto_combine.R
@@ -96,6 +96,11 @@ setMethod(
       log_date <- Sys.time()
     }
 
+    # check if ms1 and ms2 are massSight objects
+    if (!is(ms1, "MSObject") || !is(ms2, "MSObject")) {
+      stop("ms1 and ms2 must be massSight objects")
+    }
+
     validate_parameters(iso_method, match_method, smooth_method, minimum_intensity)
 
     if (match_method == "unsupervised") {
@@ -144,58 +149,8 @@ setMethod(
       )
 
     if (!is.null(log)) {
-      final_results <- all_matched(align_obj)
-      compound_id_1 <- paste("Compound_ID", name(ms1), sep = "_")
-      compound_id_2 <- paste("Compound_ID", name(ms2), sep = "_")
-      n1 <- final_results |>
-        dplyr::filter(!is.na(!!rlang::sym(compound_id_1)) &
-          is.na(!!rlang::sym(compound_id_2))) |>
-        nrow()
-      n2 <- final_results |>
-        dplyr::filter(is.na(!!rlang::sym(compound_id_1)) &
-          !is.na(!!rlang::sym(compound_id_2))) |>
-        nrow()
-      n12 <- final_results |>
-        dplyr::filter(!is.na(!!rlang::sym(compound_id_1)) &
-          !is.na(!!rlang::sym(compound_id_2))) |>
-        nrow()
-
-      metabolite_1 <- paste("Metabolite", name(ms1), sep = "_")
-      metabolite_2 <- paste("Metabolite", name(ms2), sep = "_")
-      m_correct <- final_results |>
-        dplyr::filter(!!rlang::sym(metabolite_1) == !!rlang::sym(metabolite_2)) |>
-        nrow()
-      m_total <- final_results |>
-        dplyr::filter(!is.na(!!rlang::sym(metabolite_1)) &
-          !is.na(!!rlang::sym(metabolite_2))) |>
-        nrow()
-
-      log_results <- list(
-        "Dataset 1 Unmatched" = n1,
-        "Dataset 2 Unmatched" = n2,
-        "Matched" = n12,
-        "Correct Matched Annotated Metabolites" = m_correct,
-        "Total Matched Annotated Metabolites" = m_total,
-        "Percentage Correct Matched Annotated Metabolites" = m_correct / m_total
-      )
-
-      time_end <- Sys.time()
-
-      log_file <- list(
-        date = log_date,
-        r_version = log_r,
-        parameters = log_params,
-        results = log_results,
-        runtime = as.numeric(time_end - time_start)
-      )
-      log_file <-
-        jsonlite::toJSON(log_file, auto_unbox = TRUE, pretty = TRUE)
-
-      writeLines(log_file, log)
-
-      return(align_obj)
+      log_parameters(log, log_params, log_r, log_date, align_obj, ms1, ms2, time_start)
     }
-    return(align_obj)
   }
 )
 
--- R/helpers.R
+++ R/helpers.R
@@ -8,3 +8,54 @@ validate_parameters <-
     checkmate::assert_choice(match_method, c("supervised", "unsupervised"), .var.name = "match_method")
     checkmate::assert_numeric(minimum_intensity)
   }
+
+log_parameters <- function(log, log_params, log_r, log_date, align_obj, ms1, ms2, time_start) {
+  final_results <- all_matched(align_obj)
+  compound_id_1 <- paste("Compound_ID", name(ms1), sep = "_")
+  compound_id_2 <- paste("Compound_ID", name(ms2), sep = "_")
+  n1 <- final_results |>
+    dplyr::filter(!is.na(!!rlang::sym(compound_id_1)) &
+      is.na(!!rlang::sym(compound_id_2))) |>
+    nrow()
+  n2 <- final_results |>
+    dplyr::filter(is.na(!!rlang::sym(compound_id_1)) &
+      !is.na(!!rlang::sym(compound_id_2))) |>
+    nrow()
+  n12 <- final_results |>
+    dplyr::filter(!is.na(!!rlang::sym(compound_id_1)) &
+      !is.na(!!rlang::sym(compound_id_2))) |>
+    nrow()
+
+  metabolite_1 <- paste("Metabolite", name(ms1), sep = "_")
+  metabolite_2 <- paste("Metabolite", name(ms2), sep = "_")
+  m_correct <- final_results |>
+    dplyr::filter(!!rlang::sym(metabolite_1) == !!rlang::sym(metabolite_2)) |>
+    nrow()
+  m_total <- final_results |>
+    dplyr::filter(!is.na(!!rlang::sym(metabolite_1)) &
+      !is.na(!!rlang::sym(metabolite_2))) |>
+    nrow()
+
+  log_results <- list(
+    "Dataset 1 Unmatched" = n1,
+    "Dataset 2 Unmatched" = n2,
+    "Matched" = n12,
+    "Correct Matched Annotated Metabolites" = m_correct,
+    "Total Matched Annotated Metabolites" = m_total,
+    "Percentage Correct Matched Annotated Metabolites" = m_correct / m_total
+  )
+
+  time_end <- Sys.time()
+
+  log_file <- list(
+    date = log_date,
+    r_version = log_r,
+    parameters = log_params,
+    results = log_results,
+    runtime = as.numeric(time_end - time_start)
+  )
+  log_file <-
+    jsonlite::toJSON(log_file, auto_unbox = TRUE, pretty = TRUE)
+
+  writeLines(log_file, log)
+}
