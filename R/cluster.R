4874cac438667fb38cd7971180dccfc424a2c1ed
Chiraag <37530396+chi-raag@users.noreply.github.com>
2025-06-09 12:47:44 -0400

    Deleted non-python related files

--- R/cluster.R
+++
@@ -1,7 +0,0 @@
-#' @export
-#' @title Cluster
-#' @description This function will cluster the data based on the clustering
-#' method you choose.
-cluster <- function() {
-
-}
--- R/massSight-package.R
+++
@@ -1,9 +0,0 @@
-#' @keywords internal
-"_PACKAGE"
-
-## usethis namespace: start
-#' @importFrom Rcpp sourceCpp
-#' @importFrom rlang .data
-#' @useDynLib massSight, .registration = TRUE
-## usethis namespace: end
-NULL
